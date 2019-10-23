c64f7f017325c5feafdcdcc70523414338d21558
Tushar Desai <tushardesai@Tushars-MacBook-Pro.local>
2019-10-23 10:40:23 -0400

    removes virtualenv

--- myprojectenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/mnt/c/Users/icett/OneDrive/Documents/all_things_code/projects/Quest_2.0/Akamai_Quest_Extension_Training_Application/myprojectenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
